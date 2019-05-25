b0040799de5bc6e3e17be1dd00380f280fdcda98
Coco <chuissoon@hotmail.com>
2019-05-25 19:22:54 +0200

    idea

--- app/src/androidTest/java/com/huissoon/coco/myfirstapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/huissoon/coco/myfirstapplication/ExampleInstrumentedTest.java
@@ -20,6 +20,7 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
+        //Check of gitignore werkt
 
         assertEquals("com.huissoon.coco.myfirstapplication", appContext.getPackageName());
     }
